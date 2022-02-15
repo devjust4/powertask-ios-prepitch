6322291c1a1deb20adf33437e49f8c08e5455fb4
Daniel Torres <hola@kurokiji.com>
2022-02-15 21:16:54 +0100

    agregados protocolos para arrastrar y soltar

---
+++ Powertask/Views/Calendars/Cells/TimeTableCells/SubjectDropZone.swift
@@ -0,0 +1,20 @@
+//
+//  SubjectDropZone.swift
+//  Powertask
+//
+//  Created by Daniel Torres on 15/2/22.
+//
+
+import UIKit
+
+class SubjectDropZone: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
