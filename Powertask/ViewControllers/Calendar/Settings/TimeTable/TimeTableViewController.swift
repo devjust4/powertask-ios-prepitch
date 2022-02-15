a0d59b12e874e15b69fb4609026021fc9e73999b
Daniel Torres <hola@kurokiji.com>
2022-02-15 22:21:39 +0100

    agregada función de arrastrar y soltar, solo con colores

--- Powertask/ViewControllers/Calendar/Settings/TimeTable/TimeTableViewController.swift
+++ Powertask/ViewControllers/Calendar/Settings/TimeTable/TimeTableViewController.swift
@@ -46,6 +46,20 @@ extension TimeTableViewController: UICollectionViewDragDelegate, UICollectionVie
         //}
     }
     
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: UIColor.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: UIColor.self) { colorObject in
+            let color = colorObject as! [UIColor]
+            self.view.backgroundColor = color.first
+        }
+    }
 }
 
 extension TimeTableViewController: UICollectionViewDataSource {
--- Powertask/Views/Calendars/Cells/TimeTableCells/TimeTableTableViewCell.swift
+++ Powertask/Views/Calendars/Cells/TimeTableCells/TimeTableTableViewCell.swift
@@ -12,7 +12,8 @@ class TimeTableTableViewCell: UITableViewCell {
     @IBOutlet weak var subjectDropZone: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        let dropInteraction = UIDropInteraction(delegate: self)
+        subjectDropZone.addInteraction(dropInteraction)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,4 +22,25 @@ class TimeTableTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension TimeTableTableViewCell: UIDropInteractionDelegate {
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidEnter session: UIDropSession) {
+        print("enter")
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: UIColor.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: UIColor.self) { colorObject in
+            let color = colorObject as! [UIColor]
+            self.subjectDropZone.backgroundColor = color.first
+        }
+    }
 }
